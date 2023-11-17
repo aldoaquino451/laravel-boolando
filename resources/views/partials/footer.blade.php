f214304792a03fa498585067be07f9e023ffccf7
aldoaquino451 <aldoaquino2909@gmail.com>
2023-11-17 16:27:31 +0100

    [add] header and footer [add] scaffolding

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="container">
+
+        <nav>
+            <h3>titolo</h3>
+            <ul>
+                <li> link</li>
+                <li> link</li>
+                <li> link</li>
+                <li> link</li>
+            </ul>
+        </nav>
+
+        <nav>
+            <h3>titolo</h3>
+            <ul>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+                <li>link</li>
+            </ul>
+        </nav>
+
+    </div>
+</footer>
+
+
+{{-- <footer>
+    <div class="container">
+
+      <nav>
+        <h3>{{ menuLeft.title }}</h3>
+        <ul>
+          <li
+          v-for="(item, index) in menuLeft.links" :key="index"
+          >
+            <a :href="item.link">
+              {{ item.text }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <nav>
+        <h3>{{ menuRight.title }}</h3>
+        <ul>
+          <li
+          v-for="(item, index) in menuRight.links" :key="index"
+          >
+            <a :href="item.link">
+              <i :class="item.icon"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+    </div>
+  </footer> --}}
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header>
+    <div class="container">
+
+        <nav>
+            <ul>
+                <li>link</li>
+            </ul>
+        </nav>
+
+        <div class="image">
+            <img src="../img/boolean-logo.png" alt="logo">
+        </div>
+
+        <nav>
+            <ul>
+                <li>link</li>
+            </ul>
+        </nav>
+
+    </div>
+</header>
+
+
+{{-- <header>
+    <div class="container">
+
+      <nav>
+        <ul>
+          <li
+          v-for="(item, index) in menuLeft" :key="index"
+          >
+            <a :href="item.link">
+              {{ item.text }}
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div class="image">
+        <img src="../assets/img/boolean-logo.png" alt="logo">
+      </div>
+
+      <nav>
+        <ul>
+          <li
+          v-for="(item, index) in menuRight" :key="index"
+          >
+            <a :href="item.link">
+              <i :class="item.icon"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+    </div>
+  </header> --}}
